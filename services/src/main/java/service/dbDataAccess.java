bb5e05a8ab279c5fc09da75ed689b58f65ee3f01
ggevorgyan <gevorg.gevorgyan@rovicorp.com>
2015-09-10 21:39:42 -0700

    Comiled dbAccess with get user by Id

--- services/src/main/java/service/dbDataAccess.java
+++ services/src/main/java/service/dbDataAccess.java
@@ -3,6 +3,7 @@ package service;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,6 +13,9 @@ public class dbDataAccess {
 	
 	final static Logger logger = Logger.getLogger(dbDataAccess.class);
 
+	//
+	// Get all subscribers
+	//
 	public static List<Subscriber> getSubscribers() {
 
 		List<Subscriber> results = new ArrayList<Subscriber>();
@@ -59,4 +63,63 @@ public class dbDataAccess {
 		
 		return results;
 	}
+
+	//
+	// Get subscriber by Id
+	//
+	public static Subscriber getSubscriber(int id) {
+
+		Subscriber subscriber = null;
+
+		String url = null;
+		
+		url = 
+			"jdbc:mysql://173.194.110.154:3306/Subscriptions?user=testuser&password=usertestpsw01.";
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			logger.error(e.toString());
+			
+			return subscriber;
+		}
+		PreparedStatement querySubscriber = null;
+
+		java.sql.Connection conn;
+		try {
+			logger.info("url = " + url);
+			conn = DriverManager.getConnection(url);
+
+			String query = "SELECT subscriberId, apiKey, mobileToken FROM Subscriptions.Subscriber " +
+							"WHERE id = ?";
+
+			querySubscriber = conn.prepareStatement(query);
+			querySubscriber.setInt(1, id);
+
+			ResultSet rs = querySubscriber.executeQuery();
+			
+		     while(rs.next()) {
+		         int _id = rs.getInt("subscriberId");
+		         String apiKey = rs.getString("apiKey");
+		         String mobileToken = rs.getString("mobileToken");
+		         
+		         //results.add(String.format("id [%d]/t apiKey [%s]/t mobileToken [%s]", id, apiKey, mobileToken));
+		         subscriber = new Subscriber(_id, apiKey, mobileToken);
+		         
+		      }
+		      rs.close();
+		      conn.close();
+		      conn = null;
+		 
+		} catch (SQLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+			logger.error(e1.toString());
+		}
+		
+		return subscriber;
+	}
+
 }
