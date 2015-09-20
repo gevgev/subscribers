a210228507909bc79a195819efd4e4d1d8ce6fc1
ggevorgyan <gevorg.gevorgyan@rovicorp.com>
2015-09-19 23:17:03 -0700

    Added ETag (shallow)
    Also curl -v for tests

--- services/src/main/java/service/ServiceConfiguration.java
+++ services/src/main/java/service/ServiceConfiguration.java
@@ -2,12 +2,19 @@ package service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 @SpringBootApplication
 public class ServiceConfiguration {
 
 	// private static final Logger log = LoggerFactory.getLogger(ServiceConfiguration.class);
 
+	@Bean
+    public ShallowEtagHeaderFilter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+	
     public static void main(String[] args) {
         SpringApplication.run(ServiceConfiguration.class, args);
     }
